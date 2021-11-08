f974f4971b343289a5a388c00d70082af10f7f44
Alan Jimenez <c-alan.jimenez@limeade.com>
2021-11-08 13:10:41 -0500

    Updated structure for the challenges

---
+++ challenges/01-Playing-with-digits/01-Playing-with-digits.js
@@ -0,0 +1,44 @@
+/**
+ * Playing with digits
+ *
+ * Given a positive integer **n** written as **abcd** (where every number is a letter in that order)
+ * and positive integer **p**, find the positive integer **k** (if exists) as the **sum of the digits**
+ * of **n** taken to the **successive powers** of **p** is equal to **k * n**. In other words:
+ *
+ * Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+ * If it is the case we will return k, if not return -1.
+*/
+
+function digPow(n, p) {
+   // Use absolute values
+   n = Math.abs(n)
+   p = Math.abs(p)
+
+   let numbers = n.toString().split("")
+   let sum = 0
+   let nextP = p
+
+   for (let index = 0; index < numbers.length; index++) {
+      nextP = p + index
+      const currentNumber = numbers[index];
+      let powResult = Math.pow(currentNumber, nextP)
+      sum = sum + powResult
+   }
+
+   let k = parseInt(sum / n)
+   return k === 0 ? -1 : k
+}
+
+
+// Tests
+// Expected result:   1
+digPow(89, 1)    //?
+
+// Expected result:   -1
+digPow(92, 1)    //?
+
+// Expected result:   2
+digPow(695, 2)   //?
+
+// Expected result:   51
+digPow(46288, 3) //?
