05080a348933d91afbd9a26a951f3166d42733d1
Alan Jimenez <c-alan.jimenez@limeade.com>
2021-11-09 17:22:42 -0500

    Added kata 10

---
+++ challenges/10-Duplicate-encoder/10-Duplicate-encoder.js
@@ -0,0 +1,36 @@
+/**
+ * Duplicate Encoder
+ *
+ * Convert a string to a new string, replacing each unique character with a "(".
+ * If the character appears twice or more, replace it with a ")".
+ * Ignore capitalization.
+*/
+function replace(char, word) {
+   const UNIQUE_CHAR = "("
+   const REPEATED_CHAR = ")"
+
+   let REGEX = new RegExp(`([${char}]).*\\1`, "gi")
+   if (REGEX.test(word)) {
+      return REPEATED_CHAR
+   }
+   return UNIQUE_CHAR
+}
+
+function duplicateEncode(word) {
+   let finalstring = word.replace(/./gi, char => replace(char, word));
+
+   return finalstring
+}
+
+
+// Should return:               (((
+duplicateEncode("din")     //?
+
+// Should return:               ()()()
+duplicateEncode("recede")  //?
+
+// Should return:               )())())
+duplicateEncode("Success") //?
+
+// Should return:               ))((
+duplicateEncode("(( @")    //?
