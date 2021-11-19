1d5ca6baa1f5c4e63b59627c1a1e863b8a95fd8f
Alan Jimenez <alan.jimenez@unosquare.com>
2021-11-19 10:43:56 -0500

    ASP NetCore WEBAPI items

---
+++ NetCore/01-Course-REST/Controllers/WarehouseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unosquare.Course.REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        public WarehouseController()
+        {
+
+        }
+
+        [HttpGet("GetWarehouse")]
+        public IActionResult GetWarehouse()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
